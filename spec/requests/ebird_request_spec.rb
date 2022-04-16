dc35bfec58a0fc9cea578019b9ec28dc59aff561
Ethan-bradley <30478290+Ethan-bradley@users.noreply.github.com>
2022-04-15 21:47:28 -0700

    Update ebird_request_spec.rb

--- spec/requests/ebird_request_spec.rb
+++ spec/requests/ebird_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Ebirds", type: :request do
     VCR.use_cassette('ebird/get_image_from_name') do
       VCR.use_cassette('ebird/get_bird_from_cords') do
 
-        get "/ebird", :params => { :lat => 37.42, :lng => -121.91, :radius => 25}
+        get "/ebird/birds", :params => { :lat => 37.42, :lng => -121.91, :radius => 25}
         expect(response.content_type).to eq("application/json; charset=utf-8")
         expect(response.parsed_body().first[:img]).to eq(nil)
 
